cc637a7c39fc3debbb5ea29c4e650fc1137c8537
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-08-24 15:19:40 +0100

    changed schema and color navbar

--- db/migrate/20220824105010_remove_latitude_longitude_from_surfboards.rb
+++
@@ -1,6 +0,0 @@
-class RemoveLatitudeLongitudeFromSurfboards < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :surfboards, :latitude, :float
-    remove_column :surfboards, :longitude, :float
-  end
-end

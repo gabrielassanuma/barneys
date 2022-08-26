aaa6c010c134b55cb5b498f284b518560019b3c7
Brian Allemeier <brian.all09@protonmail.com>
2022-08-26 09:56:41 +0100

    index layout fixed

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,12 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-<<<<<<< HEAD
-=======
-const track = document.querySelector('.track');
-
-const carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-
->>>>>>> 33345f942c40825a57483263102cfb81bf038496
 export default class extends Controller {
 
   connect() {
@@ -14,7 +7,6 @@ export default class extends Controller {
   }
 
   prev() {
-<<<<<<< HEAD
     const track = document.querySelector('.track');
     const cardWidth = document.querySelector('.card-container').offsetWidth;
     track.style.transform = `translateX(-${cardWidth}px)`;
@@ -24,12 +16,7 @@ export default class extends Controller {
     const track = document.querySelector('.track');
     const cardWidth = document.querySelector('.card-container').offsetWidth;
     track.style.transform = `translateX(${cardWidth}px)`;
-=======
     track.style.transform = `translateX(-${carouselWidth}px)`;
   }
 
-  next() {
-    track.style.transform = `translateX(${carouselWidth}px)`;
->>>>>>> 33345f942c40825a57483263102cfb81bf038496
-  }
 }
